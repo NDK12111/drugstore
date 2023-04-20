9a41eb3cb39b49a3ab5f5b557a8d93d36cb30ab5
NDK12111 <khangnguyen18072000@gmail.com>
2023-04-20 14:25:59 +0700

    update

---
+++ DrugStore/src/drugstore/DrugStore.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package drugstore;
+
+import drugstore.layout.formQuanLy;
+
+/**
+ *
+ * @author pjpjl
+ */
+public class DrugStore {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new formQuanLy().setVisible(true);
+    }
+    
+}
